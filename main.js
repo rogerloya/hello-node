f010b47b06cb31eb3dc392c0b9c833c0e042d5f0
rogerloya <31164770+rogerloya@users.noreply.github.com>
2021-09-20 19:34:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('Hello World!')
+console.log('HH   HH        lll lll                                  lll      dd !!! 
+HH   HH   eee  lll lll  oooo   ww      ww  oooo  rr rr  lll      dd !!! 
+HHHHHHH ee   e lll lll oo  oo  ww      ww oo  oo rrr  r lll  dddddd !!! 
+HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd     
+HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!!                                                                         
+')
